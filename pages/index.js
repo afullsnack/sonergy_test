a73ac092254d42ca51a17c2e2ddf3f627a8df6de
Mimi <miraclef60@gmail.com>
2022-06-08 19:40:58 +0100

    CHORE: Setup, installation of tailwindcss and configurations

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <h1 className="text-3xl font-bold underline">Hello, Everything</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
