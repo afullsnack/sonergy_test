19d2697ac3bef515b757a3eb7e7c0297b40f5e15
Mimi <miraclef60@gmail.com>
2022-06-22 15:55:31 +0100

    FEAT: built out the index.js page for Desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
 module.exports = {
+  mode: "jit",
+  // purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "media",
   theme: {
+    screens: {
+      mobile: "375px",
+      desktop: "1248px",
+    },
+    extend: {
+      colors: {
+        primary: "rgba(0, 89, 172, 1)",
+        secondary: "rgba(247, 250, 252, 1)",
+      },
+      backgroundImage: (theme) => ({
+        "logo-primary": "url('/SONERGY_LOGO_1.png')",
+      }),
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
