03e63a639382051d8a7e4e4a5920e506ba611304
Mimi <miraclef60@gmail.com>
2022-06-30 12:56:18 +0100

    CHORE: Fixed default export bug in login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,72 +1,11 @@
-import Button from "../../components/Button";
 import withLayout from "../../components/Layout";
-import OnboardCard from "../../components/OnboardCard";
 
 
  function Login() {
 
   return (
-    <div className="w-[100%]">
-      <OnboardCard>
-
-        {/* <Input placeholder="Username"  /> */}
-        {/* <input type="text" placeholder="Daisy UI input" className="input input-bordered bg-transparent" /> */}
-        <Button text="CLick me" onClick={() => {}} type="small" />
-      </OnboardCard>
-      <OnboardCard>
-
-        {/* <Input placeholder="Username"  /> */}
-        {/* <input type="text" placeholder="Daisy UI input" className="input input-bordered bg-transparent" /> */}
-        <Button text="CLick me" onClick={() => {}} type="small" />
-      </OnboardCard>
-      <OnboardCard>
-
-        {/* <Input placeholder="Username"  /> */}
-        {/* <input type="text" placeholder="Daisy UI input" className="input input-bordered bg-transparent" /> */}
-        <Button text="CLick me" onClick={() => {}} type="small" />
-      </OnboardCard>
-      <OnboardCard>
-
-        {/* <Input placeholder="Username"  /> */}
-        {/* <input type="text" placeholder="Daisy UI input" className="input input-bordered bg-transparent" /> */}
-        <Button text="CLick me" onClick={() => {}} type="small" />
-      </OnboardCard>
-      <OnboardCard>
-
-        {/* <Input placeholder="Username"  /> */}
-        {/* <input type="text" placeholder="Daisy UI input" className="input input-bordered bg-transparent" /> */}
-        <Button text="CLick me" onClick={() => {}} type="small" />
-      </OnboardCard>
-      <OnboardCard>
-
-        {/* <Input placeholder="Username"  /> */}
-        {/* <input type="text" placeholder="Daisy UI input" className="input input-bordered bg-transparent" /> */}
-        <Button text="CLick me" onClick={() => {}} type="small" />
-      </OnboardCard>
-      <OnboardCard>
-
-        {/* <Input placeholder="Username"  /> */}
-        {/* <input type="text" placeholder="Daisy UI input" className="input input-bordered bg-transparent" /> */}
-        <Button text="CLick me" onClick={() => {}} type="small" />
-      </OnboardCard>
-      <OnboardCard>
-
-        {/* <Input placeholder="Username"  /> */}
-        {/* <input type="text" placeholder="Daisy UI input" className="input input-bordered bg-transparent" /> */}
-        <Button text="CLick me" onClick={() => {}} type="small" />
-      </OnboardCard>
-      <OnboardCard>
-
-        {/* <Input placeholder="Username"  /> */}
-        {/* <input type="text" placeholder="Daisy UI input" className="input input-bordered bg-transparent" /> */}
-        <Button text="CLick me" onClick={() => {}} type="small" />
-      </OnboardCard>
-      <OnboardCard>
-
-        {/* <Input placeholder="Username"  /> */}
-        {/* <input type="text" placeholder="Daisy UI input" className="input input-bordered bg-transparent" /> */}
-        <Button text="CLick me" onClick={() => {}} type="small" />
-      </OnboardCard>
+    <div className="w-full">
+      
     </div>
     
     
