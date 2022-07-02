9b84106aa0f4e9208513652990bd430c4e2a387e
Mimi <miraclef60@gmail.com>
2022-07-02 18:34:04 +0100

    FEAT: Created the register mutation and tested

--- lib/mutations.tsx
+++ lib/mutations.tsx
@@ -6,4 +6,12 @@ export interface LoginData {
   password: string
 }
 
+export interface RegisterData {
+  fullName: string,
+  email: string,
+  password: string,
+  username: string,
+}
+
 export const loginUser = async (data: LoginData) => await fetch(`${BASE_URL}/auth/login`, { method: 'POST', body: JSON.stringify(data), headers: {'Content-Type': 'application/json'} }).then(res => res.json());
+export const registerUser = async (data: RegisterData) => await fetch(`${BASE_URL}/auth/register`, { method: 'POST', body: JSON.stringify(data), headers: {'Content-Type': 'application/json'} }).then(res => res.json());
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,10 +1,37 @@
 import Link from "next/link";
+import { useState } from "react";
 import { FaEnvelope, FaEyeSlash, FaLock, FaUser } from "react-icons/fa";
+import { useMutation, useQueryClient } from "react-query";
 import { ButtonPrimary } from "../components/Button";
 import Logo from "../components/Logo";
 import OnboardCard from "../components/OnboardCard";
+import { RegisterData, registerUser } from "../lib/mutations";
 
 function NewAccount () {
+
+  /**
+   * @Page => Register page
+   * @States => username, email, password, isHidden, isTOCChecked
+   * @Event => Call loginUser() mutation function and store the user token in localStorage/cookie
+  */
+   const queryClient = useQueryClient();
+
+   /* States */
+   const [username, setUsername] = useState<string>();
+   const [email, setEmail] = useState<string>();
+   const [password, setPassword] = useState<string>();
+   const [isHidden, setIsHidden] = useState<boolean>(true);
+   const [isTOCChecked, setIsTOCChecked] = useState<boolean>(false);
+ 
+   // TODO: setup useMutation
+   const { mutate, isLoading, data } = useMutation(registerUser, {
+     onSuccess: data => {
+       console.log(data, "Returned register data");
+     },
+     onError: () => console.error('There was an error trying to register'),
+     onSettled: () => queryClient.invalidateQueries('register'),
+   });
+
   return (
     <div className="container">
       <div className="w-[100%] mobile:bg-white desktop:bg-transparent flex items-center mobile:justify-between desktop:justify-center justify-center pr-4 mb-2">
@@ -31,6 +58,11 @@ function NewAccount () {
               type="text"
               placeholder="Username"
               className="input input-bordered bg-transparent text-black outline-none border-none after:ring-0 before:ring-0 before:ring-offset-0 after:ring-offset-0 pl-1 w-[100%]"
+              value={username}
+              onChange={(e) => {
+                console.info('Username', e.target.value);
+                setUsername(e.target.value);
+              }}
             />
             {/* <span>USD</span> */}
           </label>
@@ -46,9 +78,14 @@ function NewAccount () {
               <FaEnvelope color="#B8C4CE" />
             </span>
             <input
-              type="text"
+              type="email"
               placeholder="example@gmail.com"
               className="input input-bordered bg-transparent text-black outline-none border-none after:ring-0 before:ring-0 before:ring-offset-0 after:ring-offset-0 pl-1 w-[100%]"
+              value={email}
+              onChange={(e) => {
+                console.info('Email address', e.target.value);
+                setEmail(e.target.value);
+              }}
             />
             {/* <span>USD</span> */}
           </label>
@@ -62,25 +99,37 @@ function NewAccount () {
               <FaLock color="#B8C4CE" />
             </span>
             <input
-              type="text"
+              type={`${isHidden? 'password' : 'text'}`}
               placeholder="Enter password"
               className="input input-bordered bg-transparent text-black outline-none border-none after:ring-0 before:ring-0 before:ring-offset-0 after:ring-offset-0 pl-1 w-[100%]"
+              value={password}
+              onChange={(e) => {
+                console.info('Password', e.target.value);
+                setPassword(e.target.value);
+              }}
             />
             <span
               className="flex items-center justify-center pr-4 pl-1 bg-transparent"
-              onClick={(e) => console.log(e, "Eye slash click")}
+              onClick={(e) => {
+                console.log(e, "Eye slash click");
+                setIsHidden(!isHidden);
+              }}
             >
               <FaEyeSlash color="#8895A7" />
             </span>
           </label>
         </div>
         <div className="form-control">
-          <label className="cursor-pointer label">
+          <label className="cursor-pointer label flex items-center justify-center">
             <input
               type="checkbox"
               // checked="checked"
-              onChange={(e) => console.log(e, "Checkbox changed")}
-              className="checkbox checkbox-sm rounded-md checkbox-primary"
+              checked={isTOCChecked}
+              onChange={(e) => {
+                console.log(e.target.checked, "TOC Checkbox changed");
+                setIsTOCChecked(e.target.checked);
+              }}
+              className="checkbox checkbox-sm rounded-md checked:bg-primary"
             />
             <span className="label-text text-xs ml-2">
               By creating an account, you agree to our
@@ -89,12 +138,39 @@ function NewAccount () {
           </label>
         </div>
 
+        {
+          data && !data?.success && (
+            <div id="toast-warning" className="flex items-center w-full p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+              <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
+                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd"></path></svg>
+              </div>
+              <div className="ml-3 text-sm font-normal">{data?.message}</div>
+              <button type="button" className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-warning" aria-label="Close">
+                  <span className="sr-only">Close</span>
+                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+              </button>
+            </div>
+          )
+        }
+
         <ButtonPrimary
           text="Create account"
           icon={null}
           iconPosition={null}
-          onClick={(e) => console.info("Button comp clicked", e)}
-          isLoading={null}
+          onClick={async (e) => {
+            console.info("Button comp clicked", e);
+            const data: RegisterData = {
+              fullName: `Test user`,
+              username: username,
+              email: email,
+              password: password,
+            };
+
+            console.info('Form Data', data);
+
+            mutate(data);
+          }}
+          isLoading={isLoading}
           type={"normal"}
           block={false}
         />
