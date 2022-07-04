7452446764325d3c617c3e12d4da757ebe35948b
Mimi <miraclef60@gmail.com>
2022-07-04 16:44:51 +0100

    FEAT: Building out the market place pages and interactions

--- pages/home/my-surveys.tsx
+++ pages/home/my-surveys.tsx
@@ -1,22 +1,33 @@
 import { AiFillClockCircle } from "react-icons/ai";
 import { FaFileDownload } from "react-icons/fa";
-import { ButtonGhost, ButtonIcon, ButtonPrimary } from "../../components/Button";
+import {
+  ButtonGhost,
+  ButtonIcon,
+  ButtonPrimary,
+} from "../../components/Button";
 import withLayout from "../../components/Layout";
 import OnboardCard from "../../components/OnboardCard";
 
-
-function MySurveys () {
+function MySurveys() {
   return (
     <div className="w-full">
       {/* Sort actions */}
       <div className="flex flex-col items-start justify-start w-full bg-white mobile:p-3 mb-10">
         <div className="flex items-center justify-center w-[100%] mb-2">
-          <button className="flex-1 mx-1 p-2 rounded-md border-solid border-[1px] border-slate-200 mobile:text-xs">Commissioned</button>
-          <button className="flex-1 mx-1 p-2 rounded-md border-solid border-[1px] border-slate-200 mobile:text-xs">Completed</button>
-          <button className="flex-1 mx-1 p-2 rounded-md border-solid border-[1px] border-slate-200 mobile:text-xs">Validated</button>
+          <button className="flex-1 mx-1 p-2 rounded-md border-solid border-[1px] border-slate-200 mobile:text-xs">
+            Commissioned
+          </button>
+          <button className="flex-1 mx-1 p-2 rounded-md border-solid border-[1px] border-slate-200 mobile:text-xs">
+            Completed
+          </button>
+          <button className="flex-1 mx-1 p-2 rounded-md border-solid border-[1px] border-slate-200 mobile:text-xs">
+            Validated
+          </button>
         </div>
         <select className="select select-bordered w-full bg-gray-200 text-slate-700">
-          <option disabled selected>Filter</option>
+          <option disabled selected>
+            Filter
+          </option>
           <option>Last 7 days</option>
           <option>Last 30 days</option>
           <option>Last 3 months</option>
@@ -30,26 +41,63 @@ function MySurveys () {
         <OnboardCard>
           <div className="border-solid border-[1px] border-slate-200 rounded-lg p-4 mb-3">
             <div className="flex flex-col items-start justify-between mb-3">
-              <div className="badge bg-orange-200 text-orange-700 text-xs border-none mb-3">Pending completion</div>
-              <span className="text-gray-700 text-sm font-normal text-left mb-2">Blockchain development and utilisation in sub-saharan Africa.</span>
-              <p className="flex items-center justify-center"><b className="text-gray-700 text-mf font-medium">200 SNEGY</b> </p>
-              <p className="flex items-center justify-center"> <AiFillClockCircle size={14} /><span className="text-xs text-gray-500 ml-1">Expires in 2 days 11 hours</span> </p>
+              <div className="badge bg-orange-200 text-orange-700 text-xs border-none mb-3">
+                Pending completion
+              </div>
+              <span className="text-gray-700 text-sm font-normal text-left mb-2">
+                Blockchain development and utilisation in sub-saharan Africa.
+              </span>
+              <p className="flex items-center justify-center">
+                <b className="text-gray-700 text-mf font-medium">200 SNEGY</b>{" "}
+              </p>
+              <p className="flex items-center justify-center">
+                {" "}
+                <AiFillClockCircle size={14} />
+                <span className="text-xs text-gray-500 ml-1">
+                  Expires in 2 days 11 hours
+                </span>{" "}
+              </p>
             </div>
             <div className="flex items-center justify-between space-x-4">
               <div className="flex-[3]">
-                <ButtonGhost text="Convert to NFT" type="normal" icon={null} iconPosition={null} block={true} onClick={e => console.log('Convert to NFT', e)} />
+                <ButtonGhost
+                  text="Convert to NFT"
+                  type="normal"
+                  icon={null}
+                  iconPosition={null}
+                  block={true}
+                  onClick={(e) => console.log("Convert to NFT", e)}
+                  isLoading={false}
+                />
               </div>
               <div className="flex-1">
-                <ButtonIcon type="normal" icon={<FaFileDownload size={18} />} block={true} onClick={e => console.log('Download', e)} />
+                <ButtonIcon
+                  type="normal"
+                  icon={<FaFileDownload size={18} />}
+                  block={true}
+                  onClick={(e) => console.log("Download", e)}
+                />
               </div>
             </div>
           </div>
           <div className="border-solid border-[1px] border-slate-200 rounded-lg p-4 mb-3">
             <div className="flex flex-col items-start justify-between mb-3">
-              <div className="badge bg-green-200 text-green-700 text-xs border-none mb-3">Completed</div>
-              <span className="text-gray-700 text-sm font-normal text-left mb-2">Blockchain development and utilisation in sub-saharan Africa.</span>
-              <p className="flex items-center justify-center"><b className="text-gray-700 text-mf font-medium">200 SNEGY</b> </p>
-              <p className="flex items-center justify-center"> <AiFillClockCircle size={14} /><span className="text-xs text-gray-500 ml-1">Expires in 2 days 11 hours</span> </p>
+              <div className="badge bg-green-200 text-green-700 text-xs border-none mb-3">
+                Completed
+              </div>
+              <span className="text-gray-700 text-sm font-normal text-left mb-2">
+                Blockchain development and utilisation in sub-saharan Africa.
+              </span>
+              <p className="flex items-center justify-center">
+                <b className="text-gray-700 text-mf font-medium">200 SNEGY</b>{" "}
+              </p>
+              <p className="flex items-center justify-center">
+                {" "}
+                <AiFillClockCircle size={14} />
+                <span className="text-xs text-gray-500 ml-1">
+                  Expires in 2 days 11 hours
+                </span>{" "}
+              </p>
             </div>
             {/* <div className="flex items-center justify-between space-x-4">
               <div className="flex-[3]">
@@ -65,7 +113,15 @@ function MySurveys () {
 
       {/* Analyze action buttn */}
       <div className="flex flex-col items-start justify-start bg-white w-full mobile:p-3">
-        <ButtonPrimary type="normal" text="Analyze survey" icon={undefined} iconPosition={undefined} block={true} onClick={e => console.log(e, "Analyze survey")} />
+        <ButtonPrimary
+          type="normal"
+          text="Analyze survey"
+          icon={undefined}
+          iconPosition={undefined}
+          block={true}
+          onClick={(e) => console.log(e, "Analyze survey")}
+          isLoading={false}
+        />
       </div>
     </div>
   );
--- pages/market/index.tsx
+++ pages/market/index.tsx
@@ -1,19 +1,95 @@
+import { useState } from "react";
 import withLayout from "../../components/Layout";
-import OnboardCard from "../../components/OnboardCard";
-
 
 function Market() {
+  const [sort, setSort] = useState<string>("marketplace");
 
   return (
     <div className="w-full">
-      <div className="flex flex-col items-start justify-start w-full bg-transparent mobile:p-3 mb-8">
-        <OnboardCard>
-          <h2>Market Page Comming soon...</h2>
-        </OnboardCard>
+      <div className="flex flex-col items-start justify-start w-full bg-white p-3 mb-8">
+        {/* Search action */}
+        <form className="form-control mb-4 w-full">
+          {/* <label
+              htmlFor="search"
+              className="mb-4 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+            >
+              Your Email
+            </label> */}
+          <div className="relative">
+            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+              <svg
+                className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                ></path>
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="search"
+              className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder={`Search ${
+                sort === "marketplace"
+                  ? "marketplace"
+                  : sort === "collections"
+                  ? "my collections"
+                  : "completed surveys"
+              }...`}
+              required
+            />
+            {/* <button
+                type="submit"
+                className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button> */}
+          </div>
+        </form>
+        {/* Sort actions */}
+        {/* <div className="flex flex-col items-start justify-start w-full bg-white mb-10"> */}
+        <div className="flex items-center justify-center w-full mb-2">
+          <button
+            className={`flex-1 mr-1 p-2 rounded-md border-solid border-[1px] ${
+              sort === "marketplace"
+                ? "border-primary bg-[#B9D0E3] opacity-50 text-gray-800"
+                : "border-slate-200"
+            } mobile:text-xs`}
+            onClick={(e) => setSort("marketplace")}
+          >
+            Marketplace
+          </button>
+          <button
+            className={`flex-1 mx-1 p-2 rounded-md border-solid border-[1px] ${
+              sort === "collections"
+                ? "border-primary bg-[#B9D0E3] opacity-50 text-gray-800"
+                : "border-slate-200"
+            }  mobile:text-xs`}
+            onClick={(e) => setSort("collections")}
+          >
+            My collections
+          </button>
+          <button
+            className={`flex-1 ml-1 p-2 rounded-md border-solid border-[1px] ${
+              sort === "completed"
+                ? "border-primary bg-[#B9D0E3] opacity-50 text-gray-800"
+                : "border-slate-200"
+            }  mobile:text-xs`}
+            onClick={(e) => setSort("completed")}
+          >
+            Completed
+          </button>
+        </div>
       </div>
     </div>
   );
 }
-
 
 export default withLayout(Market);
