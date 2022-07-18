221c7e12e4c2a7ddef5057fb726d03aebf0bc15a
Mimi <miraclef60@gmail.com>
2022-07-18 20:28:50 +0100

    CHORE: Exposed env variables to client

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    IPFS_URL: process.env.IPFS_URL,
+    IPFS_PROJEC_ID: process.env.IPFS_PROJEC_ID,
+    IPFS_PROJECT_SECRET: process.env.IPFS_PROJECT_SECRET,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
